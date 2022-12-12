4f62b2165ecb514e1cae79316e4ba97ea802bfee
easylearningbd <elebdsoft@gmail.com>
2022-12-13 02:50:29 +0600

    19 How to Display Login Credentials Message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,13 +55,21 @@
             <div class="mb-3">
                 <label for="login" class="form-label">Email/Name/Phone</label>
 
-                <input class="form-control" name="login" type="text" id="login" required="" placeholder="Enter your email">
+ <input class="form-control @error('login') is-invalid @enderror" name="login" type="text" id="login" required="" placeholder="Enter your email">
+ @error('login')
+ <span class="text-danger"> {{ $message }} </span>
+ @enderror
             </div>
 
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
                 <div class="input-group input-group-merge">
-                    <input type="password" id="password" name="password" class="form-control" placeholder="Enter your password">
+                    <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Enter your password">
+   @error('password')
+ <span class="text-danger"> {{ $message }} </span>
+ @enderror
+
+
                     <div class="input-group-text" data-password="false">
                         <span class="password-eye"></span>
                     </div>
