88e63558912d1335bc3b1f53b275e11f5a00e320
easylearningbd <elebdsoft@gmail.com>
2022-12-13 03:46:41 +0600

    22 How to Protect URL By Middleware

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,16 @@ Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('adm
 
 Route::get('/logout', [AdminController::class, 'AdminLogoutPage'])->name('admin.logout.page');
 
+
+Route::middleware(['auth'])->group(function(){ 
+
 Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 
 Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+ 
 
 Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
 
 Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+
+}); // End User Middleware 
