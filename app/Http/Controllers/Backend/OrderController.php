bdc9a96049e641d906f6b6eafc10d146480927cf
easylearningbd <elebdsoft@gmail.com>
2023-01-13 04:00:37 +0600

    89 Create Order  System Part 3

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -9,12 +9,53 @@ use App\Models\Customer;
 use App\Models\Order;
 use App\Models\Orderdetails;
 use Carbon\Carbon; 
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class OrderController extends Controller
 {
 
-    
+
     public function FinalInvoice(Request $request){
+
+        $data = array();
+        $data['customer_id'] = $request->customer_id;
+        $data['order_date'] = $request->order_date;
+        $data['order_status'] = $request->order_status;
+        $data['total_products'] = $request->total_products;
+        $data['sub_total'] = $request->sub_total;
+        $data['vat'] = $request->vat;
+
+        $data['invoice_no'] = 'EPOS'.mt_rand(10000000,99999999);
+        $data['total'] = $request->total;
+        $data['payment_status'] = $request->payment_status;
+        $data['pay'] = $request->pay;
+        $data['due'] = $request->due;
+        $data['created_at'] = Carbon::now(); 
+
+        $order_id = Order::insertGetId($data);
+        $contents = Cart::content();
+
+        $pdata = array();
+        foreach($contents as $content){
+            $pdata['order_id'] = $order_id;
+            $pdata['product_id'] = $content->id;
+            $pdata['quantity'] = $content->qty;
+            $pdata['unitcost'] = $content->price;
+            $pdata['total'] = $content->total;
+            
+            $insert = Orderdetails::insert($pdata); 
+
+        } // end foreach
+
+
+        $notification = array(
+            'message' => 'Order Complete Successfully',
+            'alert-type' => 'success'
+        );
+
+        Cart::destroy();
+
+        return redirect()->route('dashboard')->with($notification);
 
     } // End Method 
 
