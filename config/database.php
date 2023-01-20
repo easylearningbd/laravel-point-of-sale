d9862f6d12ef7baa6a27c23d32a37778f3491f0e
easylearningbd <elebdsoft@gmail.com>
2023-01-21 02:39:57 +0600

    121 How to Backup Database Part 1

--- config/database.php
+++ config/database.php
@@ -58,6 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'dump' => [
+            'dump_binary_path' => 'C:/localhost/mysql/bin', //  
+            'use_single_transaction',
+            'timeout' => 60 * 5, // 5 minute timeout             
+            ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
