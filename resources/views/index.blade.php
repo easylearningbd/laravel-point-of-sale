b1ccd6fa9311eade8e4e534d2eaa266d2ccbdc82
easylearningbd <elebdsoft@gmail.com>
2023-01-22 04:36:23 +0600

    129 Admin Dashboar Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,19 @@
 @extends('admin_dashboard')
 @section('admin')
 
+@php
+ $date = date('d-F-Y');
+ $today_paid = App\Models\Order::where('order_date',$date)->sum('pay');
+
+ $total_paid = App\Models\Order::sum('pay');
+ $total_due = App\Models\Order::sum('due'); 
+
+ $completeorder = App\Models\Order::where('order_status','complete')->get(); 
+
+  $pendingorder = App\Models\Order::where('order_status','pending')->get(); 
+
+@endphp
+
  <div class="content">
 
                     <!-- Start Content-->
@@ -32,87 +45,87 @@
                         </div>     
                         <!-- end page title --> 
 
-                        <div class="row">
-                            <div class="col-md-6 col-xl-3">
-                                <div class="widget-rounded-circle card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <div class="avatar-lg rounded-circle bg-primary border-primary border shadow">
-                                                    <i class="fe-heart font-22 avatar-title text-white"></i>
-                                                </div>
-                                            </div>
-                                            <div class="col-6">
-                                                <div class="text-end">
-                                                    <h3 class="text-dark mt-1">$<span data-plugin="counterup">58,9</span></h3>
-                                                    <p class="text-muted mb-1 text-truncate">Total Revenue</p>
-                                                </div>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div>
-                                </div> <!-- end widget-rounded-circle-->
-                            </div> <!-- end col-->
+<div class="row">
+<div class="col-md-6 col-xl-3">
+    <div class="widget-rounded-circle card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <div class="avatar-lg rounded-circle bg-primary border-primary border shadow">
+                        <i class="fe-heart font-22 avatar-title text-white"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="text-end">
+                        <h3 class="text-dark mt-1">$<span data-plugin="counterup">{{ $total_paid }}</span></h3>
+                        <p class="text-muted mb-1 text-truncate">Total Paid </p>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+        </div>
+    </div> <!-- end widget-rounded-circle-->
+</div> <!-- end col-->
 
-                            <div class="col-md-6 col-xl-3">
-                                <div class="widget-rounded-circle card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <div class="avatar-lg rounded-circle bg-success border-success border shadow">
-                                                    <i class="fe-shopping-cart font-22 avatar-title text-white"></i>
-                                                </div>
-                                            </div>
-                                            <div class="col-6">
-                                                <div class="text-end">
-                                                    <h3 class="text-dark mt-1"><span data-plugin="counterup">127</span></h3>
-                                                    <p class="text-muted mb-1 text-truncate">Today's Sales</p>
-                                                </div>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div>
-                                </div> <!-- end widget-rounded-circle-->
-                            </div> <!-- end col-->
+<div class="col-md-6 col-xl-3">
+    <div class="widget-rounded-circle card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <div class="avatar-lg rounded-circle bg-success border-success border shadow">
+                        <i class="fe-shopping-cart font-22 avatar-title text-white"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                      <div class="text-end">
+                        <h3 class="text-dark mt-1">$<span data-plugin="counterup">{{ $total_due  }}</span></h3>
+                        <p class="text-muted mb-1 text-truncate">Total Due </p>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+        </div>
+    </div> <!-- end widget-rounded-circle-->
+</div> <!-- end col-->
 
-                            <div class="col-md-6 col-xl-3">
-                                <div class="widget-rounded-circle card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <div class="avatar-lg rounded-circle bg-info border-info border shadow">
-                                                    <i class="fe-bar-chart-line- font-22 avatar-title text-white"></i>
-                                                </div>
-                                            </div>
-                                            <div class="col-6">
-                                                <div class="text-end">
-                                                    <h3 class="text-dark mt-1"><span data-plugin="counterup">0.58</span>%</h3>
-                                                    <p class="text-muted mb-1 text-truncate">Conversion</p>
-                                                </div>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div>
-                                </div> <!-- end widget-rounded-circle-->
-                            </div> <!-- end col-->
+<div class="col-md-6 col-xl-3">
+    <div class="widget-rounded-circle card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <div class="avatar-lg rounded-circle bg-info border-info border shadow">
+                        <i class="fe-bar-chart-line- font-22 avatar-title text-white"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                   <div class="text-end">
+                        <h3 class="text-dark mt-1"> <span data-plugin="counterup">{{ count($completeorder)  }}</span></h3>
+                        <p class="text-muted mb-1 text-truncate">Complete Order </p>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+        </div>
+    </div> <!-- end widget-rounded-circle-->
+</div> <!-- end col-->
 
-                            <div class="col-md-6 col-xl-3">
-                                <div class="widget-rounded-circle card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-6">
-                                                <div class="avatar-lg rounded-circle bg-warning border-warning border shadow">
-                                                    <i class="fe-eye font-22 avatar-title text-white"></i>
-                                                </div>
-                                            </div>
-                                            <div class="col-6">
-                                                <div class="text-end">
-                                                    <h3 class="text-dark mt-1"><span data-plugin="counterup">78.41</span>k</h3>
-                                                    <p class="text-muted mb-1 text-truncate">Today's Visits</p>
-                                                </div>
-                                            </div>
-                                        </div> <!-- end row-->
-                                    </div>
-                                </div> <!-- end widget-rounded-circle-->
-                            </div> <!-- end col-->
-                        </div>
+<div class="col-md-6 col-xl-3">
+    <div class="widget-rounded-circle card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <div class="avatar-lg rounded-circle bg-warning border-warning border shadow">
+                        <i class="fe-eye font-22 avatar-title text-white"></i>
+                    </div>
+                </div>
+                <div class="col-6">
+                   <div class="text-end">
+                        <h3 class="text-dark mt-1"> <span data-plugin="counterup">{{ count($pendingorder)  }}</span></h3>
+                        <p class="text-muted mb-1 text-truncate">Pending Order </p>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+        </div>
+    </div> <!-- end widget-rounded-circle-->
+</div> <!-- end col-->
+</div>
                         <!-- end row-->
 
                         <div class="row">
